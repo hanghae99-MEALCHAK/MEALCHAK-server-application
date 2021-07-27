3bf21e3626b8d45eaf7670001757213fcc0e8c27
Tbread <81889764+Tbread@users.noreply.github.com>
2021-07-27 21:01:14 +0900

    WebSocketConfig 수정

--- src/main/java/com/mealchak/mealchakserverapplication/config/WebSocketConfig.java
+++ src/main/java/com/mealchak/mealchakserverapplication/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.mealchak.mealchakserverapplication.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -8,19 +9,20 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
-        //구독용
-        registry.enableSimpleBroker("/topic");
-        //메세지를 주고받을 url
-        registry.setApplicationDestinationPrefixes("/app");
+        //구독용(sub)
+        registry.enableSimpleBroker("/sub");
+        //발행용(pub)
+        registry.setApplicationDestinationPrefixes("/pub");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         //나중에 allowed origin 수정예정
         //웹소켓 연결 url
-        registry.addEndpoint("/chatting").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*").withSockJS();
     }
 }
