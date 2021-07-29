4f7d735cf7307a62676a95630dfafb1481b581aa
Tbread <81889764+Tbread@users.noreply.github.com>
2021-07-29 11:56:10 +0900

    페이지 리네이밍 및 메시지매핑에서 토큰 접근하려던점 수정

--- src/main/java/com/mealchak/mealchakserverapplication/controller/ChatMessageController.java
+++ src/main/java/com/mealchak/mealchakserverapplication/controller/ChatMessageController.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -19,7 +21,7 @@ public class ChatMessageController {
 
 
     @MessageMapping("/message")
-//    public void message(@AuthenticationPrincipal UserDetailsImpl userDetails,@RequestBody ChatMessageRequestDto messageRequestDto){
+//    public void message(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody ChatMessageRequestDto messageRequestDto){
     public void message(@RequestBody ChatMessageRequestDto messageRequestDto) {
         //임시로 주석처리
 //        messageRequestDto.setSender(userDetails.getUser().getUsername());
