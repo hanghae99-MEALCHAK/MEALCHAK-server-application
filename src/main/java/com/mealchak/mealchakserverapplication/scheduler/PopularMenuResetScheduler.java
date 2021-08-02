da138a8bf5641b4640db47cb54886ecac574fd9e
Tbread <81889764+Tbread@users.noreply.github.com>
2021-08-02 20:34:36 +0900

    스케쥴러 쓰레드 다중실행을 위해 어플리케이션에 Async 어노테이션 추가

---
+++ src/main/java/com/mealchak/mealchakserverapplication/scheduler/PopularMenuResetScheduler.java
@@ -0,0 +1,9 @@
+package com.mealchak.mealchakserverapplication.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PopularMenuResetScheduler {
+}
