5f325bd0618d11d4fcdf2d1bc0ebbf146d79dba5
Tbread <81889764+Tbread@users.noreply.github.com>
2021-08-02 20:35:20 +0900

    PostExpiredScheduler 와 다른 스케쥴러의 동시 실행을 위해 Async 어노테이션 추가

--- src/main/java/com/mealchak/mealchakserverapplication/scheduler/PostExpiredScheduler.java
+++ src/main/java/com/mealchak/mealchakserverapplication/scheduler/PostExpiredScheduler.java
@@ -3,6 +3,7 @@ package com.mealchak.mealchakserverapplication.scheduler;
 import com.mealchak.mealchakserverapplication.model.Post;
 import com.mealchak.mealchakserverapplication.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,7 @@ public class PostExpiredScheduler {
 
     @Scheduled(fixedDelay = 60 * 1000L)
     @Transactional
+    @Async
     public void postValidationCheckScheduler() {
         List<Post> notExpiredList = postRepository.findByCheckValid(true);
         for (Post post : notExpiredList) {
