bcc664841a209cc0f50169a1485b02d38e88d0af
Tbread <81889764+Tbread@users.noreply.github.com>
2021-08-02 20:33:57 +0900

    스케쥴러 쓰레드 다중실행을 위한 SchedulerConfig 추가

---
+++ src/main/java/com/mealchak/mealchakserverapplication/config/SchedulerConfig.java
@@ -0,0 +1,25 @@
+package com.mealchak.mealchakserverapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    //스케쥴러 쓰레드 개수
+    private final static int THREAD_SIZE = 2;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar){
+
+        final ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+
+        threadPoolTaskScheduler.setPoolSize(THREAD_SIZE);
+        threadPoolTaskScheduler.setThreadNamePrefix("Scheduler-");
+        threadPoolTaskScheduler.initialize();
+
+        taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
+}
