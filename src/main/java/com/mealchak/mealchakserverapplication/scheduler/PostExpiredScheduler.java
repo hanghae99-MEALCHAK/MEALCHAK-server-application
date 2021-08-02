9ea7420d24d9421968dd4da760db1bf6347cafc6
Tbread <81889764+Tbread@users.noreply.github.com>
2021-08-02 17:39:21 +0900

    쓰지않는 import 삭제

--- src/main/java/com/mealchak/mealchakserverapplication/scheduler/PostExpiredScheduler.java
+++ src/main/java/com/mealchak/mealchakserverapplication/scheduler/PostExpiredScheduler.java
@@ -4,7 +4,6 @@ import com.mealchak.mealchakserverapplication.model.Post;
 import com.mealchak.mealchakserverapplication.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
