3501abd5696bc91542a85855ab3cf60452279815
Tbread <81889764+Tbread@users.noreply.github.com>
2021-08-03 17:51:29 +0900

    게시글 (채팅방) 입장 승인/비승인 로직 추가

--- src/main/java/com/mealchak/mealchakserverapplication/repository/UserRepository.java
+++ src/main/java/com/mealchak/mealchakserverapplication/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.mealchak.mealchakserverapplication.repository;
 
 import com.mealchak.mealchakserverapplication.model.User;
+import com.mealchak.mealchakserverapplication.repository.mapping.UserInfoMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +10,5 @@ public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByKakaoId(Long kakaoId);
     <T> Optional<T> findByEmail(String email, Class <T>type);
+    <T> Optional<T> findById(Long userId, Class <T>type);
 }
