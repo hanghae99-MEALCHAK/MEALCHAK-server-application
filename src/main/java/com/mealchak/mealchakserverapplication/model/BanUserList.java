979775b5c771be0b49d5d9dd4eeee843b79ee491
Tbread <81889764+Tbread@users.noreply.github.com>
2021-08-05 15:27:13 +0900

    채팅방 유저 추방기능 추가

---
+++ src/main/java/com/mealchak/mealchakserverapplication/model/BanUserList.java
@@ -0,0 +1,29 @@
+package com.mealchak.mealchakserverapplication.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class BanUserList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long roomId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Builder
+    public BanUserList(Long roomId,Long userId){
+        this.roomId = roomId;
+        this.userId = userId;
+    }
+
+}
--- src/main/java/com/mealchak/mealchakserverapplication/model/ChatMessage.java
+++ src/main/java/com/mealchak/mealchakserverapplication/model/ChatMessage.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class ChatMessage extends Timestamped {
 
     public enum MessageType {
-        ENTER, TALK, QUIT
+        ENTER, TALK, QUIT, BAN
     }
 
     @Id
