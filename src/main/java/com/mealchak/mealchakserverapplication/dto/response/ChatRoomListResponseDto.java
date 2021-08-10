a953e573875934caa6ac453c2d39e9d7a545e22e
Gorani92 <peses0326@gmail.com>
2021-08-10 15:41:50 +0900

    현재 유저의 채팅방 목록 조회시 채팅방 title 정보 추가 전송

--- src/main/java/com/mealchak/mealchakserverapplication/dto/response/ChatRoomListResponseDto.java
+++ src/main/java/com/mealchak/mealchakserverapplication/dto/response/ChatRoomListResponseDto.java
@@ -4,21 +4,17 @@ import com.mealchak.mealchakserverapplication.model.ChatRoom;
 import com.mealchak.mealchakserverapplication.model.Post;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 public class ChatRoomListResponseDto {
-    private final LocalDateTime createdAt;
-    private final LocalDateTime modifiedAt;
     private final String title;
+    private final String orderTime;
     private final Long ownUserId;
     private final Long roomId;
     private final Long postId;
     private final Long headCountChat;
 
     public ChatRoomListResponseDto(ChatRoom chatRoom, Post post, Long headCountChat) {
-        this.createdAt = chatRoom.getCreatedAt();
-        this.modifiedAt = chatRoom.getModifiedAt();
+        this.orderTime = post.getOrderTime();
         this.title = post.getTitle();
         this.ownUserId = chatRoom.getOwnUserId();
         this.roomId = chatRoom.getId();
