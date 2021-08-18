3ac4cab35c4c7ddc8f1b7a17667ead4bba62c147
Tbread <81889764+Tbread@users.noreply.github.com>
2021-08-18 16:03:54 +0900

    PostRepository 재생성 (cud를 위함)

---
+++ src/main/java/com/mealchak/mealchakserverapplication/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.mealchak.mealchakserverapplication.repository;
+
+import com.mealchak.mealchakserverapplication.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Long> {
+}
