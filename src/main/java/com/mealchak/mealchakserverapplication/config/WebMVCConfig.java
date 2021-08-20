69f7272a336cd7daae8d6943f41dc68b4ce3afdd
Tbread <81889764+Tbread@users.noreply.github.com>
2021-08-20 13:05:20 +0900

    이미지를 내려줄때 캐시관련 헤더와 함께 내려가도록 변경

--- src/main/java/com/mealchak/mealchakserverapplication/config/WebMVCConfig.java
+++ src/main/java/com/mealchak/mealchakserverapplication/config/WebMVCConfig.java
@@ -4,9 +4,12 @@ import com.mealchak.mealchakserverapplication.util.CORSFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
 
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
@@ -26,8 +29,10 @@ public class WebMVCConfig implements WebMvcConfigurer {
     // 경로 수정 필요
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        CacheControl cacheControl = CacheControl.noCache().mustRevalidate().cachePrivate().sMaxAge(Duration.ZERO);
         registry.addResourceHandler("/image/**")
 //                .addResourceLocations("file:/home/ubuntu/image/");  // AWS EC2
-                  .addResourceLocations("file:/root/image/");   // NAVER EC2
+                .addResourceLocations("file:/root/image/")   // NAVER EC2
+                .setCacheControl(cacheControl);
     }
 }
